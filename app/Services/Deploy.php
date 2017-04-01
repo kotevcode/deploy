ecd26addc169753626f59c1e872c3c8748959f6a
Shahaf Antwarg <santwarg@gmail.com>
2017-04-01 16:55:38 +0300

    initial

--- app/Services/Deploy.php
+++ app/Services/Deploy.php
@@ -55,7 +55,7 @@ class Deploy {
 
     // Discard any changes to tracked files since our last deploy
     exec('git reset --hard HEAD', $output);
-    $this->log('Reseting repository... ');
+    $this->log('Reseting repository... ', implode(' ', $output));
 
     // Update the local repository
     exec('git pull '.$this->_repository->remote.' '.$this->_repository->branch, $output);
