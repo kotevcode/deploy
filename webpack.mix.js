bfeef0c787809278b3560df26befc97da33555e3
Shahaf Antwarg <santwarg@gmail.com>
2017-04-01 09:13:03 +0300

    browser sync support

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,24 @@
 const { mix } = require('laravel-mix');
-
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
+mix.webpackConfig({
+  plugins: [
+    new BrowserSyncPlugin({
+      open: 'external',
+      host: 'localhost',
+      proxy: '/deploy/public',
+      files: ['resources/views/**/*.php', 'app/**/*.php', 'routes/**/*.php']
+    })
+  ]
+});
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+.sass('resources/assets/sass/app.scss', 'public/css');
