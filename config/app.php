777f3eb5f297d2ba3d5d3c4e3841a072cb9a1673
Shahaf Antwarg <santwarg@gmail.com>
2017-04-02 20:25:14 +0300

    modifying

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Deploy by Shahaf Antwarg',
+    'name' => 'Deploy',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Jerusalem',
 
     /*
     |--------------------------------------------------------------------------
